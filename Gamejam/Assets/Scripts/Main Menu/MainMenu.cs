fb9c8b9034591ac907358a4e5fe97234c4fd753d
Matej Kuprešak <matej.kupresak@gmail.com>
2022-08-23 23:25:31 +0200

    update, main menu

---
+++ Gamejam/Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
