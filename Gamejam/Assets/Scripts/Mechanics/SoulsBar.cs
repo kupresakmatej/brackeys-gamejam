4bead7c179a87b1279af67f89e271e943bdf00bc
Matej Kuprešak <matej.kupresak@gmail.com>
2022-08-26 12:50:13 +0200

    update, player

--- Gamejam/Assets/Scripts/Mechanics/SoulsBar.cs
+++ Gamejam/Assets/Scripts/Mechanics/SoulsBar.cs
@@ -17,7 +17,7 @@ public class SoulsBar : MonoBehaviour
     private Light lightIntensity;
 
     [SerializeField]
-    private float loseLight = 0.0001f;
+    private float loseLight = 0.00001f;
 
     private PostProcessVolume v;
     public static Vignette vignette;
@@ -42,15 +42,15 @@ public class SoulsBar : MonoBehaviour
         {
             lightBar.fillAmount -= (loseLight + 0.05f) * Time.deltaTime;
 
-            lightIntensity.intensity = light_left - (loseLight + 0.05f) * Time.deltaTime;
-            vignette.intensity.value += (loseLight + 0.05f) / 2.5f * Time.deltaTime;
+            lightIntensity.intensity = light_left - (loseLight + 0.05f)/2 * Time.deltaTime;
+            vignette.intensity.value += (loseLight + 0.05f) / 10f * Time.deltaTime;
         }
         else
         {
             lightBar.fillAmount -= loseLight * Time.deltaTime;
 
             lightIntensity.intensity = light_left - loseLight * Time.deltaTime;
-            vignette.intensity.value += loseLight / 2.5f * Time.deltaTime;
+            vignette.intensity.value += loseLight / 10f * Time.deltaTime;
         }
 
         light_left = lightBar.fillAmount;
