9cf6f4fcc578f879b39e4e625f9262285bbcb734
Matej Kuprešak <matej.kupresak@gmail.com>
2022-08-27 17:08:47 +0200

    update

---
+++ Gamejam/Assets/Scripts/Mechanics/IntroCar.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroCar : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject black;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
